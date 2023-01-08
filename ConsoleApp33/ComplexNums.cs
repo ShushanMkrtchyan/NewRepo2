62d4957b75a5c6fe15086a8fdce4bda5efdb0c3e
ShushanMkrtchyan <shushmk01@outlook.com>
2023-01-08 17:05:01 +0400

    Add project files.

---
+++ ConsoleApp33/ComplexNums.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp33
+{
+    
+        class ComplexNums
+        {
+            int real, imag;
+
+            public ComplexNums(int r, int i)
+            {
+                real = r;
+                imag = i;
+
+            }
+
+            public void Show()
+            {
+                Console.WriteLine(real + "+" + imag + "*i");
+            }
+
+            public static ComplexNums operator +(ComplexNums num1, ComplexNums num2)
+            {
+                ComplexNums num3 = new ComplexNums(num1.real + num2.real, num1.imag + num2.imag);
+                return num3;
+            }
+
+            public static ComplexNums operator -(ComplexNums num1, ComplexNums num2)
+            {
+                ComplexNums num3 = new ComplexNums(num1.real - num2.real, num1.imag - num2.imag);
+                return num3;
+            }
+
+            public static bool operator >(ComplexNums num1, ComplexNums num2)
+            {
+                return num1.real * num2.real > num1.imag * num2.imag;
+
+            }
+
+            public static bool operator <(ComplexNums num1, ComplexNums num2)
+            {
+                return num1.real * num2.real < num1.imag * num2.imag;
+            }
+
+            public static bool operator ==(ComplexNums num1, ComplexNums num2)
+            {
+                return num1.real == num2.real && num1.imag == num2.imag;
+            }
+
+            public static bool operator !=(ComplexNums num1, ComplexNums num2)
+            {
+                return num1.real != num2.real && num1.imag != num2.imag;
+            }
+
+
+            public override bool Equals(object obj)
+            {
+                if (obj == null || GetType() != obj.GetType())
+                {
+                    return false;
+                }
+                ComplexNums c = obj as ComplexNums;
+                return c.real == this.real && c.imag == this.imag;
+            }
+
+            public override int GetHashCode()
+            {
+                return real ^ imag;
+            }
+
+
+            public static bool operator true(ComplexNums c)
+            {
+                return c.real != 0 || c.imag != 0;
+            }
+
+            public static bool operator false(ComplexNums c)
+            {
+                return c.real != 0 || c.imag != 0;
+            }
+
+            public static ComplexNums operator |(ComplexNums c1, ComplexNums c2)
+            {
+                if ((c1.real != 0 && c1.imag != 0) | (c2.real != 0 && c2.imag != 0))
+                    return new ComplexNums(2, 2);
+                else
+                    return new ComplexNums(0, 0);
+
+
+            }
+
+            public static ComplexNums operator &(ComplexNums c1, ComplexNums c2)
+            {
+                if ((c1.real != 0 && c1.imag != 0) | (c2.real != 0 && c2.imag != 0))
+                    return new ComplexNums(2, 2);
+                else
+                    return new ComplexNums(0, 0);
+
+            }
+
+            public static explicit operator string(ComplexNums c)
+
+            {
+                return c.real.ToString();
+
+            }
+
+        }
+    }
+
---
+++ ConsoleApp33/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace ConsoleApp33
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ComplexNums comp1 = new ComplexNums(7, 8);
+            ComplexNums comp2 = new ComplexNums(5, 6);
+
+            ComplexNums comp3 = comp1 + comp2;
+            comp3.Show();
+
+            ComplexNums comp4 = comp1 - comp2;
+            comp4.Show();
+
+
+            if (comp1 > comp2)
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine("false");
+            }
+
+            if (comp1 == comp2)
+            {
+                Console.WriteLine("Condition is true");
+            }
+
+            else
+            {
+                Console.WriteLine("Condition is false");
+            }
+
+            if (comp1)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+
+            var result = comp1 | comp2;
+
+            if (result)
+            {
+                result.Show();
+            }
+            else
+            {
+                Console.WriteLine("Try again");
+            }
+
+            var result1 = comp1 | comp2;
+
+            if (result1)
+            {
+                result1.Show();
+            }
+            else
+            {
+                Console.WriteLine("Try again");
+            }
+
+            string value = (string)comp1;
+
+            Console.WriteLine(value);
+        }
+    }
+}
